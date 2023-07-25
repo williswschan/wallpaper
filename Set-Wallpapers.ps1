22ea8aeabe7deaa9f9cde80986ec7c8e7e68b234
Willis Chan <89602886+williswschan@users.noreply.github.com>
2023-07-25 12:17:26 +0800

    Add files via upload

--- Set-Wallpapers.ps1
+++ Set-Wallpapers.ps1
@@ -6,6 +6,7 @@ For ($MonitorID = 0; $MonitorID -le $((Get-ConnectedMonitors) - 1); $MonitorID++
     $Width = (Get-MonitorResolution -MonitorID $MonitorID).Width
     $Height = (Get-MonitorResolution -MonitorID $MonitorID).Height
     $WallpaperFilePath = $WallpaperDirectory + "\img0_" + $Width + "x" + $Height + ".jpg"
+    $WallpaperFilePath = "C:\Windows\Web\4K\Wallpaper\Windows\img0_3840x2160.jpg"
     
     If ([System.IO.File]::Exists($WallpaperFilePath)) {
         Set-Wallpaper -MonitorID $MonitorID -WallpaperFilePath $WallpaperFilePath
